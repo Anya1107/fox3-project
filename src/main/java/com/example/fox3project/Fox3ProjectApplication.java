01cfa4d43415a7576cfa10100b234ab38737b261
Anya1107 <murzenok.anya@mail.ru>
2021-07-02 22:32:16 +0300

    first commit

---
+++ src/main/java/com/example/fox3project/Fox3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fox3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fox3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fox3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fox3project/Fox3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fox3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fox3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
